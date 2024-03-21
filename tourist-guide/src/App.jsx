d417588c97b9dd0361f3c446894fccbffd81c47e
PanavShah1 <panav.shah@gmail.com>
2024-03-21 14:17:38 +0530

    boilerplate

---
+++ tourist-guide/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Map from './components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <Map apikey="3c6lSSEiWgHS8hTMQIGALMvXZBnYTSwE-st42tatJWc"/>
+    </div>
+  );
+}
+
+export default App;
